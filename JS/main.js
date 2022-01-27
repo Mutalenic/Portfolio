8ee10abbe74f8ef88a7fbb7992d38e5d813e92f8
Nicholas Mutale <nicomutale@gmail.com>
2022-01-27 16:41:58 +0200

    make website dynamic

--- JS/main.js
+++ JS/main.js
@@ -20,3 +20,89 @@ links.forEach((link) => link.addEventListener('click', () => {
   menuActive.classList.remove('active');
   menuCross.classList.remove('active');
 }));
+
+const cardcontainer = document.querySelector('.lang-sec');
+
+const languages = (string1,string2,string3,string4)=>{
+  return `<li>${string1}</li>
+  <li>${string2}</li>
+  <li>${string3}</li>
+  <li class="ruby">${string4}</li>`
+}
+
+cardsinfo = [
+  {
+    reverseCard:'kwacha',
+    cardName:'Multi-Post Stories',
+    mainText: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. has been the industry's standard dummy text
+    ever since the 1500s, when an unknown printer took a standard
+    dummy text.`,
+    cardLang: languages('css','html','bootstrap','ruby'),
+    image: `./placeholder.png`,
+
+  },
+  {
+    reverseCard:'reverse-row',
+    cardName:'Multi-Post Stories',
+    mainText: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. has been the industry's standard dummy text
+    ever since the 1500s, when an unknown printer took a standard
+    dummy tex.`,
+    cardLang: languages('css','html','bootstrap','ruby'),
+    image: `./placeholder.png`,
+    
+  },
+  {
+    reverseCard:'kwacha',
+    cardName:'Multi-Post Stories',
+    mainText: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. has been the industry's standard dummy text
+    ever since the 1500s, when an unknown printer took a standard
+    dummy text.`,
+    cardLang: languages('css','html','bootstrap','ruby'),
+    image: `./placeholder.png`,
+    
+  },
+  {
+    reverseCard:'reverse-row',
+    cardName:'Multi-Post Stories',
+    mainText: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. has been the industry's standard dummy text
+    ever since the 1500s, when an unknown printer took a standard
+    dummy text.`,
+    cardLang: languages('css','html','bootstrap','ruby'),
+    image: `./placeholder.png`,
+    
+  }
+
+]
+
+function createCard(cardData){
+  cardcontainer.innerHTML = ` `;
+  cardData.forEach((card, pos) => {
+    const cardMaster = `
+    <li class="card-sec ${card.reverseCard}">
+          <div class="placeholder1">
+            <img
+              class="image1"
+              src="${card.image}"
+              alt="image placeholder"
+            />
+          </div>
+          <div class="card1">
+            <h2 class="post">${card.cardName}</h2>
+            <p class="text">
+              ${card.mainText}
+            </p>
+            <ul class="lang">
+              ${card.cardLang}  
+            </ul>
+            <button class="button pop-${pos}" type="button">See Project</button>
+          </div>
+        </li>`
+        cardcontainer.insertAdjacentHTML('afterbegin', cardMaster);
+  })
+} 
+
+createCard(cardsinfo)
